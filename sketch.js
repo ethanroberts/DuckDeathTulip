2d5b7bee6c384670a721960f6d13871857285633
Guest User <Guest@f-06-06-lab.mddn.vuw.ac.nz>
2015-04-29 22:12:36 +1200

    adjusted rotation of tulips in wind scene

--- sketch.js
+++ sketch.js
@@ -510,7 +510,7 @@ function draw() {
 			if (tulipIdleAnimPoint == tulipIdleMag || tulipIdleAnimPoint == 0) {
 				tulipIdleDir *= -1;
 			}
-			tulipRot += tulipIdleDir/20 + (accelerationZ*2);
+			tulipRot = accelerationZ*2;
 			if (tulipRot <= -20) {
 				tulipRot = -20;
 			}
